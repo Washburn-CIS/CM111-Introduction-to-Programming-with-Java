7a5f487176c6dad21e2cd8bcb31b422ea0413293
Joey Kendall-Morwick <jbmorwick@gmail.com>
2025-12-18 14:25:21 -0600

    moved out classroom resources and deleted other old artifacts

--- classroom-resources/labs/ch05/daysofmonth/Days.java
+++
@@ -1,9 +0,0 @@
-public final class Days   // FINAL CLASS CAN'T BE EXTENDED
-{
-  
-  private Days() { }     // PRIVATE CONSTRUCTOR SO CAN'T BE INSTANTIATED
-
-
-} // Days
-
-
--- classroom-resources/labs/ch12/fibonacci/FibonacciTest.java
+++
@@ -1,7 +0,0 @@
-public class FibonacciTest
-{
-	public static void main(String argv[])
-	{
-		System.out.println("fib of 4 = " + Fibonacci.fibIter1(4));
-	}
-}
