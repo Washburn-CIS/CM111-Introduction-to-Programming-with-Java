5bdfd83258009b6818a0ecc54444d00fcdb8c963
jmorwick <jbmorwick@gmail.com>
2026-01-06 17:04:07 -0600

    made some changes to the ch1 intro #12 #13 #4 #5

---
+++ pretext/ch1/code/HelloWorld.java
@@ -0,0 +1,10 @@
+/*
+ * File: HelloWorld.java
+ * Author: Joseph Kendall-Morwick
+ * Description: Prints Hello World! 
+ */
+public class HelloWorld { // Class header
+    public static void main(String args[]) { // Method header
+        System.out.println("Hello World!"); // Output greeting
+    }  // End main()
+} // End class body
